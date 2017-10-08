26e2e81cfd56f6999f4427ad82c3f1707e7a093a
tutstu <ashurenu1993@gmail.com>
2017-10-09 00:33:16 +0530

    css linking fixed again

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'pre-exams.herokuapp.com';
+$config['base_url'] = 'https://pre-exams.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
